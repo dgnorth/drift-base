8f916f49c95033a48d5ec8c48098f5e6eeb99d6a
Matthias Gudmundsson <matti@directivegames.com>
2016-09-28 13:37:55 +0000

    Amendment to last commit. basicConfig was called in the most inapproproate manner.

--- flask_uwsgi.py
+++ flask_uwsgi.py
@@ -3,7 +3,7 @@ import os
 from os.path import join, abspath
 import logging
 
-logging.basicConfig('INFO')
+logging.basicConfig(level='INFO')
 
 config_file = abspath(join(__file__, '../config', 'config.json'))
 config_file = os.path.abspath(config_file)
