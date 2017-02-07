de12fa0943f78a1d52ddb1e7d7eff14ab6d8e9d5
Matthias Gudmundsson <mattigumm@gmail.com>
2017-02-07 09:32:22 +0000

    Removing "drift_CONFIG" initialization from kitrun.py.

--- kitrun.py
+++ kitrun.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python
-import os
-import sys
-from os.path import join, abspath
 from drift import management
-
-config_file = abspath(join(__file__, "../config", "config.json"))
-os.environ.setdefault("drift_CONFIG", config_file)
 
 if __name__ == "__main__":
     management.execute_cmd()
