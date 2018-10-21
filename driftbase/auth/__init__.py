57ba102506c638eb2daced4149e7f85c3f561d29
Kristján Valur Jónsson <sweskman@gmail.com>
2018-10-21 15:05:36 +0000

    driftbase.auth is now a resource

--- driftbase/auth/__init__.py
+++ driftbase/auth/__init__.py
@@ -1,6 +1,31 @@
-# -*- coding: utf-8 -*-
+import importlib
 
 from drift.utils import get_config
+from drift.core.extensions import jwt
+
+from . import authenticate
+
+
+AUTH_MODULES = {
+    'gamecenter': 'driftbase.auth.gamecenter',
+    'googleplay': 'driftbase.auth.googleplay',
+    'oculus': 'driftbase.auth.oculus',
+    'psn': 'driftbase.auth.psn',
+    'steam': 'driftbase.auth.steam',
+    }
+
+LOCAL_AUTH = [
+    'device_id', 'user+pass', 'uuid', 'unit_test', 'viveport', 'hypereal', '7663',
+    ]
+
+
+def drift_init_extension(app, api, **kwds):
+    # register authentication handlers
+    for name, module in AUTH_MODULES.items():
+        m = importlib.import_module(module)
+        jwt.register_auth_provider(app, name, m.authenticate)
+    for name in LOCAL_AUTH:
+        jwt.register_auth_provider(app, name, authenticate.authenticate_with_provider)
 
 
 def get_provider_config(provider_name):
--- driftbase/auth/authenticate.py
+++ driftbase/auth/authenticate.py
@@ -1,43 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import logging
-import importlib
 
 from six.moves import http_client
-
-from werkzeug.security import pbkdf2_hex
 
 from flask import g, current_app
 from flask_restplus import abort
 from click import secho
 
-from drift.core.extensions import jwt
-
 from driftbase.models.db import User, CorePlayer, UserIdentity, UserRole
 from driftbase.utils import UserCache
 
 log = logging.getLogger(__name__)
-
-AUTH_MODULES = {
-    'gamecenter': 'driftbase.auth.gamecenter',
-    'googleplay': 'driftbase.auth.googleplay',
-    'oculus': 'driftbase.auth.oculus',
-    'psn': 'driftbase.auth.psn',
-    'steam': 'driftbase.auth.steam',
-    }
-
-LOCAL_AUTH = [
-    'device_id', 'user+pass', 'uuid', 'unit_test', 'viveport', 'hypereal', '7663',
-    ]
-
-
-def drift_init_extension(app, api, **kwds):
-    # register authentication handlers
-    for name, module in AUTH_MODULES.items():
-        m = importlib.import_module(module)
-        jwt.register_auth_provider(app, name, m.authenticate)
-    for name in LOCAL_AUTH:
-        jwt.register_auth_provider(app, name, authenticate_with_provider)
 
 
 def abort_unauthorized(description):
