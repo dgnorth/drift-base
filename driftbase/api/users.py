c89e26f9d7f3fb45cda00688dd714edbcf493181
Jon Bjarnason <nonnib@gmail.com>
2018-10-24 22:59:51 +0000

    flask-rest-api experiments

--- driftbase/api/users.py
+++ driftbase/api/users.py
@@ -3,7 +3,10 @@ from six.moves import http_client
 
 from flask import url_for, g
 from flask_restplus import Namespace, Resource, abort
-
+from flask.views import MethodView
+import marshmallow as ma
+from flask_rest_api import Api, Blueprint
+from marshmallow_sqlalchemy import ModelSchema
 from drift.core.extensions.urlregistry import Endpoints
 from driftbase.utils import url_player, url_user
 from driftbase.models.db import User, CorePlayer, UserIdentity
@@ -11,43 +14,71 @@ from driftbase.models.db import User, CorePlayer, UserIdentity
 log = logging.getLogger(__name__)
 
 
-namespace = Namespace("users", "User management")
 endpoints = Endpoints()
 
+bp = Blueprint('users', 'Users', url_prefix='/users', description='User management')
+
+class PlayerSchema(ModelSchema):
+    class Meta:
+        model = CorePlayer
+        exclude = ('num_logons', )
+    player_url = ma.fields.Str(description="Hello")
+
+class UserSchema(ModelSchema):
+
+    class Meta:
+        model = User
+    user_url = ma.fields.Str(description="Hello")
+    client_url = ma.fields.Str()
+    user_url = ma.fields.Str()
+    players = ma.fields.List(ma.fields.Nested(PlayerSchema))
+    identities = ma.fields.List(ma.fields.Dict())
+
+class UserRequestSchema(ma.Schema):
+
+    class Meta:
+        strict = True
+        ordered = True
 
 def drift_init_extension(app, api, **kwargs):
-    api.add_namespace(namespace)
     endpoints.init_app(app)
 
+    api.spec.definition('User', schema=UserSchema)
 
-@namespace.route('', endpoint='users')
-class UsersListAPI(Resource):
-    """
-    list users
-    """
+    api.register_blueprint(bp)
+
+
+#@namespace.route('', endpoint='users')
+@bp.route('', endpoint='users')
+class UsersListAPI(MethodView):
+    @bp.response(UserSchema(many=True))
     def get(self):
-        """
+        """List Users
 
+        Return users, just the most recent 10 records with no paging or anything I'm afraid.
         """
         ret = []
         users = g.db.query(User).order_by(-User.user_id).limit(10)
         for row in users:
             user = {
                 "user_id": row.user_id,
-                "user_url": url_user(row.user_id)
+                "user_url": url_for('users.user', user_id=row.user_id, _external=True)
             }
             ret.append(user)
         return ret
 
 
-@namespace.route('/<int:user_id>', endpoint='user')
+#@namespace.route('/<int:user_id>', endpoint='user')
+@bp.route('/<int:user_id>', endpoint='user')
 class UsersAPI(Resource):
     """
 
     """
+    @bp.response(UserSchema(many=False))
     def get(self, user_id):
-        """
+        """A single user
 
+        Return a user by ID
         """
         user = g.db.query(User).filter(User.user_id == user_id).first()
         if not user:
@@ -55,24 +86,12 @@ class UsersAPI(Resource):
 
         data = user.as_dict()
         data["client_url"] = None
-        if user.client_id:
-            data["client_url"] = url_for("client", client_id=user.client_id, _external=True)
+        #if user.client_id:
+        #    data["client_url"] = url_for("client", client_id=user.client_id, _external=True)
         players = g.db.query(CorePlayer).filter(CorePlayer.user_id == user_id)
-        data["players"] = []
-        for player in players:
-            data["players"].append({"player_id": player.player_id,
-                                    "player_name": player.player_name,
-                                    "player_url": url_player(player.player_id)
-                                    })
+        data["players"] = players
         identities = g.db.query(UserIdentity).filter(UserIdentity.user_id == user_id)
-        data["identities"] = []
-        for identity in identities:
-            data["identities"].append({"identity_id": identity.identity_id,
-                                       "name": identity.name,
-                                       "type": identity.identity_type,
-                                       "logon_date": identity.logon_date,
-                                       "num_logons": identity.num_logons,
-                                       })
+        data["identities"] = identities
 
         return data
 
