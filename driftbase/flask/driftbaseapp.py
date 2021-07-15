95064d618f2014b41f36481adf1c54eeb4c4ee51
Andreas Axelsson <axl@directivegames.com>
2021-07-15 11:48:39 +0800

    Include ddtrace library for DataDog APM support
    - ENABLE_DATADOG_APM=1 to enable

--- driftbase/flask/driftbaseapp.py
+++ driftbase/flask/driftbaseapp.py
@@ -1,13 +1,10 @@
 """
 Flask app with gevent monkey patching.
 """
-import logging
-
 from gevent import monkey
 monkey.patch_all()
 from psycogreen.gevent import patch_psycopg
 patch_psycopg()
 
-from drift.flaskfactory import drift_app
-
-app = drift_app()
+import drift.contrib.flask.datadogapp
+app = drift.contrib.flask.datadogapp.app
