c5e4c8ccaf97af70e15aff537cfcfd2565e71569
Einar Th. Einarsson <einsi@directivegames.com>
2021-08-27 10:27:17 +0000

    minor tweaks

--- driftbase/flexmatch.py
+++ driftbase/flexmatch.py
@@ -276,7 +276,6 @@ def _process_searching_event(event):
             _post_matchmaking_event_to_members(player_id, "MatchmakingSearching")
 
 def _process_potential_match_event(event):
-    player_ids_to_notify = set()
     players_by_team = defaultdict(set)
     players_by_ticket = defaultdict(set)  # For sanity checking
     for ticket in event["tickets"]:
@@ -370,7 +369,7 @@ def _process_matchmaking_succeeded_event(event):
             player_ticket["MatchId"] = event["matchId"]
             player_ticket["GameSessionConnectionInfo"] = copy.copy(game_session_info)
             player_ticket["GameSessionConnectionInfo"].update({
-                "ConnectionString": f"{ip_address}:{port}",
+                "ConnectionString": connection_string,
                 "ConnectionOptions": connection_info_by_player_id[player_id]
             })
             ticket_lock.ticket = player_ticket
@@ -394,7 +393,7 @@ def _process_matchmaking_cancelled_event(event):
                 if player_ticket is None:
                     continue  # Normal, player cancelled the ticket and we've already cleared it from the cache
                 if player_ticket["Status"] == "COMPLETED" and ticket_id != player_ticket["TicketId"]:
-                    # This is not a flexmatch status, but I want to differentiate between statuses arising from the
+                    # MATCH_COMPLETE is not a flexmatch status, but I want to differentiate between statuses arising from the
                     # cancelling of backfill tickets and other states
                     log.info(f"Found player {player_id} in a foreign ticket being cancelled, where the actual players ticket is in 'COMPLETED' state."
                              " Inferring a backfill ticket being cancelled and thus setting player ticket to 'MATCH_COMPLETE'.")
