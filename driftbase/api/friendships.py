c96ba93a13797e323cc3794e27d5c9c4c8423b2f
Hilmar Tryggvason <hilmart@gmail.com>
2021-11-17 11:07:27 +0000

    [friendships] Extend non-specific player friend invites
    - Return non-specific player friend invites as well when querying for the invites
    - When creating an invite, don't query for expired invites (tokens can be re-used this way without deleting the invite)
    - Add wordlist token format based on the BIP039 english wordlist
    - Add schema for creating an invite (specify token format and expiration time)
    - Move player ID from query params to request body when creating an invite
    - Add Friends resource (The Machines AR is apparently using it)
    - Add and fix tests

--- driftbase/api/friendships.py
+++ driftbase/api/friendships.py
@@ -15,8 +15,9 @@ from sqlalchemy.orm import aliased
 from driftbase.models.db import Friendship, FriendInvite, CorePlayer
 from driftbase.schemas.friendships import InviteSchema, FriendRequestSchema
 from driftbase.messages import post_message
+from driftbase.utils import wordlist
 
-DEFAULT_INVITE_EXPIRATION_TIME_SECONDS = 60 * 60 * 1
+from driftbase.resources.friends import TIER_DEFAULTS
 
 log = logging.getLogger(__name__)
 
@@ -90,12 +91,10 @@ class FriendshipsAPI(MethodView):
 
         invite_token = args.get("token")
 
-        invite = g.db.query(FriendInvite).filter_by(token=invite_token).first()
+        # Get the first non-expired invite that matches the token
+        invite = g.db.query(FriendInvite).filter(FriendInvite.token == invite_token, FriendInvite.expiry_date > datetime.datetime.utcnow()).first()
         if invite is None:
             abort(http_client.NOT_FOUND, description="The invite was not found!")
-
-        if invite.expiry_date < datetime.datetime.utcnow():
-            abort(http_client.FORBIDDEN, description="The invite has expired!")
 
         if invite.deleted:
             abort(http_client.FORBIDDEN, description="The invite has been deleted!")
@@ -165,35 +164,43 @@ class FriendshipAPI(MethodView):
 
 @bp.route('/invites', endpoint='invites')
 class FriendInvitesAPI(MethodView):
+    class CreateInviteSchema(ma.Schema):
+        player_id = ma.fields.Integer(required=False, metadata=dict(description="The receiving player of the invite. Optional."))
+        expiration_time_seconds = ma.fields.Integer(required=False, metadata=dict(description="The expiration time of the invite in seconds."))
+        token_format = ma.fields.String(required=False, metadata=dict(description="The format of the token. Supported values: 'uuid' and 'wordlist'."))
+        worldlist_number_of_words = ma.fields.Integer(required=False, metadata=dict(description="How many words the token should be if the token format is 'wordlist'"))
 
     @bp.response(http_client.OK, InviteSchema(many=True))
     def get(self):
         """ List invites sent by current player """
         CorePlayer2 = aliased(CorePlayer)
         return g.db.query(FriendInvite, CorePlayer.player_name, CorePlayer2.player_name). \
-            join(CorePlayer, CorePlayer.player_id == FriendInvite.issued_to_player_id). \
+            join(CorePlayer, CorePlayer.player_id == FriendInvite.issued_to_player_id, isouter=True). \
             join(CorePlayer2, CorePlayer2.player_id == FriendInvite.issued_by_player_id). \
             filter(FriendInvite.issued_by_player_id == int(current_user["player_id"]),
                    FriendInvite.expiry_date > datetime.datetime.utcnow(),
                    FriendInvite.deleted.is_(False))
 
-    def post(self):
+    @bp.arguments(CreateInviteSchema)
+    def post(self, args):
         """
         New Friend token
         """
         sending_player_id = int(current_user["player_id"])
         try:
-            receiving_player_id = int(request.args.get("player_id"))
+            receiving_player_id = int(args.get("player_id"))
             self._validate_friend_request(receiving_player_id)
         except TypeError:
             receiving_player_id = None
 
-        token = str(uuid.uuid4())
-        expires_seconds = DEFAULT_INVITE_EXPIRATION_TIME_SECONDS
-        config = g.conf.tenant.get('friends')
-        if config:
-            expires_seconds = config['invite_expiration_seconds']
-        expires_seconds = expires_seconds
+        token_format = args.get("token_format") or _get_tenant_config_value("invite_token_format")
+        if token_format == "uuid":
+            token = str(uuid.uuid4())
+        elif token_format == "wordlist":
+            number_of_words = args.get("worldlist_number_of_words") or _get_tenant_config_value("invite_token_worldlist_number_of_words")
+            token = wordlist.get_word_combination(number_of_words)
+
+        expires_seconds = args.get("expiration_time_seconds") or _get_tenant_config_value("invite_expiration_seconds")
         expires = datetime.datetime.utcnow() + datetime.timedelta(seconds=expires_seconds)
 
         try:
@@ -307,3 +314,12 @@ def endpoint_info(*args):
     if current_user:
         ret["my_friends"] = url_for("friendships.list", player_id=current_user["player_id"], _external=True)
     return ret
+
+# Helpers
+
+def _get_tenant_config_value(config_key):
+    default_value = TIER_DEFAULTS.get(config_key, None)
+    tenant = g.conf.tenant
+    if tenant:
+        return g.conf.tenant.get("friends", {}).get(config_key, default_value)
+    return default_value
