3e44d94685e0ebcb903bf7b6a83c77315704a59b
Andreas Axelsson <axl@directivegames.com>
2021-12-09 16:33:29 +0800

    Enable metrics before tracing
    - If using ddtrace-run, that's the order things will get set up, so
    let's see if that helps with our missing APM metrics

--- driftbase/flask/driftbaseapp.py
+++ driftbase/flask/driftbaseapp.py
@@ -1,20 +1,25 @@
 """
 Flask app with gevent monkey patching.
 """
+# Always patch_all first
 from gevent import monkey
 monkey.patch_all()
+
+# Additionally patch psycopg
 from psycogreen.gevent import patch_psycopg
 patch_psycopg()
 
 import os
 
-if os.environ.get('ENABLE_DATADOG_APM', '0') == '1':
-    import ddtrace
-    ddtrace.patch_all(logging=True)
-
+# Optionally enable Metrics
 if os.environ.get('ENABLE_DATADOG_METRICS', '0') == '1':
     from ddtrace.runtime import RuntimeMetrics
     RuntimeMetrics.enable()
+
+# Optionally enable APM
+if os.environ.get('ENABLE_DATADOG_APM', '0') == '1':
+    import ddtrace
+    ddtrace.patch_all(logging=True)
 
 from drift.flaskfactory import drift_app
 app = drift_app()
