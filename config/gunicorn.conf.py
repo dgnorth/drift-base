968f1ca3570be7e057a93f504cdbbcf6d215a145
Andreas Axelsson <axl@directivegames.com>
2022-09-02 16:54:11 +0800

    Remove default access log format, leave it up to deployment

--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -19,7 +19,7 @@ graceful_timeout=30
 keep_alive=2
 timeout=30
 
-access_log_format = '{"remote_address": "%(h)s", "@timestamp": "%(t)s", "method": "%(r)s", "status": "%(s)s"}'
+# access_log_format = '{\n\t"method (m)": "%(m)s",\n\t"path (U)": "%(U)s",\n\t"protocol (H)": "%(H)s",\n\t"request (r)": "%(r)s",\n\t"remote_address (h)": "%(h)s",\n\t"timestamp (t)": "%(t)s",\n\t"status_code (s)": "%(s)s",\n\t"response_size (B)": "%(B)i",\n\t"response_time_ms (M)": "%(M)i",\n\t"user_agent (a)": "%(a)s",\n\t"referer (f)": "%(f)s"\n}'
 
 # useful settings overrides
 # --loglevel INFO --dogstatsd-tags tag,tag,tag --backlog 2048 --workers 1 --worker-connections 1000 --keep-alive 2 --graceful-timeout 30 --timeout 30
